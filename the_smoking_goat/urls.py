f2107766f3078a44e17a56c19e6271988b9e9b9b
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-05-23 12:54:39 +0000

    Add content to home page

--- the_smoking_goat/urls.py
+++ the_smoking_goat/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('booking/', include('booking.urls')),
+    path('', include('booking.urls')),
     path('admin/', admin.site.urls),
     
 ]
