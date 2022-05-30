e9427cd3c5fdf4f4180e62534942a7fd3b0b3480
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-05-30 12:09:24 +0000

    Add models for menu and recipe

--- easy_recipe/models.py
+++ easy_recipe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class RecipePost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField("image", default="placeholder")
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
---
+++ menu/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+COURSECHOICES = [(0,'Starters'), (1, 'Mains'), (2,'Desert')]
+class FoodMenu(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    course = models.IntegerField(choices=COURSECHOICES)
+    price = models.IntegerField()
