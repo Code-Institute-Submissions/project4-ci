cbc8894b5c5b2048908595ed7c030fab3736d8bc
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-05-30 23:07:45 +0000

    add a login page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-# Create your views here.
+from django.contrib.auth.views import LoginView
 
 
 def index(request):
     return render(request, 'index.html')
 
+class LoginPage(LoginView):
+    template_name = 'login.html'
