e3df6c896c5d20ed770e963f72ef7ab53e32dca5
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-06-02 18:39:30 +0000

    add a createview page to recipes

--- easy_recipe/urls.py
+++ easy_recipe/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import RecipeCreateView
 
 urlpatterns = [
     path('', views.recipes, name='recipes'),
+    path('/create_recipe', views.RecipeCreateView.as_view(), name='create_recipe')
         ]
--- easy_recipe/views.py
+++ easy_recipe/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import RecipePost
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 @login_required(redirect_field_name='account_login')
 def recipes(request):
     recipes = RecipePost.objects.all()
     return render(request, 'easy_recipe/recipes.html', {'recipes': recipes})
+
+class RecipeCreateView(CreateView):
+    model = RecipePost 
+    template_name = 'easy_recipe/recipe_create_form.html'
+    fields = [
+        'title',
+        'author',
+        'content', 
+        'featured_image',
+        'excerpt',
+        ]
 
