8626e79406c4d53161c92340a9e6438edf40fdaf
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-06-08 13:20:36 +0000

    add styling and linebreaks to recipe details

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-
+class TimeSlot(models.Model):
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    
