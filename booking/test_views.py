fe6d66e0217b3247c8d4e06e3d83334ebd7d8fce
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-06-20 13:02:35 +0000

    Add tests to easy_recipe and booking

--- booking/test_views.py
+++ booking/test_views.py
@@ -11,14 +11,15 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        self.user = User.objects.create_superuser('admin', 'admin@mail.com', 'adminpassword')
 
-    def test_get_booking_day(self):
+    def test_get_booking_day_page(self):
         self.client.login(username='john', password='johnpassword')
         response = self.client.get(reverse('booking_day'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'booking/booking_day.html')
 
-    def test_get_booking_day(self):
+    def test_get_booking_day_unauthorize(self):
         response = self.client.get(reverse('booking_day'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/accounts/login/?next=/booking/booking_day')
@@ -30,14 +31,30 @@ class TestViews(TestCase):
     #     self.assertTemplateUsed(response, 'booking/booking_detail.html')
 
 
-    def test_get_booking_list(self):
+    def test_get_customer_booking_list_page(self):
         self.client.login(username='john', password='johnpassword')
         response = self.client.get(reverse('customer_booking_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'booking/customer_booking_list.html')
 
-    def test_get_booking_date_list_page(self):
+    def test_get_booking_date_list_page_unauthorized(self):
         response = self.client.get(reverse('booking_date_list'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/admin/login/?next=/booking/booking_date_list')
     
+    def test_get_booking_date_list_page_authorized(self):
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(reverse('booking_date_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booking/booking_date_list.html')
+
+    def test_get_closed_days_unauthorized(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('closed_list'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_closed_days_page_authorised(self):
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(reverse('closed_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booking/closed_list.html')
--- easy_recipe/test_views.py
+++ easy_recipe/test_views.py
@@ -11,14 +11,37 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        self.user = User.objects.create_superuser('admin', 'admin@mail.com', 'adminpassword')
 
-    def test_get_recipe_list(self):
+
+    def test_get_recipe_list_page_authorized_user(self):
         self.client.login(username='john', password='johnpassword')
         response = self.client.get(reverse('recipes'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'easy_recipe/recipes.html')
     
-    def test_get_recipe_create_page(self):
-        response = self.client.get(reverse('create_recipe'))
+    def test_get_recipe_list_page_not_loggedin(self):
+        response = self.client.get(reverse('recipes'))
         self.assertEqual(response.status_code, 302)
-        self.assertTemplateUsed(response, 'accounts/login.html')
+        self.assertRedirects(response, '/accounts/login/?next=/recipes/')
+    
+    def test_get_recipe_create_page_unauthorizes_user(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('create_recipe'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_recipe_edit_page_unauthorizes_user(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('create_recipe'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_recipe_delete_page_unauthorizes_user(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('create_recipe'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_recipe_create_page_authorizes_user(self):
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(reverse('create_recipe'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'easy_recipe/recipe_create_form.html')
