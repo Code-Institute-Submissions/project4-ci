7af0dd585bd338bfc1235d88b76264513f55ede7
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-06-21 12:44:01 +0000

    Add tests

--- easy_recipe/test_views.py
+++ easy_recipe/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.test.client import Client
 from django.contrib.auth.models import User
+from .models import RecipePost
 
 
 
@@ -45,3 +46,22 @@ class TestViews(TestCase):
         response = self.client.get(reverse('create_recipe'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'easy_recipe/recipe_create_form.html')
+
+    # def test_can_add_new_recipe(self):
+    #     self.client.login(username='admin', password='adminpassword')
+    #     response = self.client.post('/recipe_create_form', {'title': 'Test Added Item',
+    #                                 'author':'admin',
+    #                                 'content':'test'
+    #                                  })
+    #     self.assertRedirects(response, reverse('recipe_detail', kwargs=[self.object.id]))
+
+    # def test_can_edit_recipe_item(self):
+    #     self.client.login(username='admin', password='adminpassword')
+    #     item = RecipePost.objects.create(title='Test Added Item',
+    #                                author= User.objects.get(id=current_user.id),
+    #                                content='test'
+    #                                 )
+    #     response = self.client.post(f'/recipe_update_form/{item.id}', {'name': 'Updated Name'})
+    #     self.assertRedirects(response, reverse('recipe_detail', kwargs=[item.id]))
+    #     update_item = RecipePost.objects.get(id=item.id)
+    #     self.assertEqual(update_item.name, 'Updated Name')
--- menu/test_views.py
+++ menu/test_views.py
@@ -1,8 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test.client import Client
 
 class TestViews(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        self.user = User.objects.create_superuser('admin', 'admin@mail.com', 'adminpassword')
+
     def test_get_menu_page(self):
-        response = self.client.get('/menu_page/')
+        response = self.client.get(reverse('menu_page'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'menu/menu_page.html')
+
+    def test_get_menu_list_page_authorized(self):
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(reverse('menu_list'))
+        self.assertAlmostEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'menu/menu_list.html')
+
+    def test_get_menu_list_page_unauthorized(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('menu_list'))
+        self.assertAlmostEqual(response.status_code, 403)
+
+    # def test_get_menu_delete_item_page_unauthorizes_user(self):
+    #     self.client.login(username='john', password='johnpassword')
+    #     response = self.client.get(reverse('item_delete'))
+    #     self.assertEqual(response.status_code, 403)
+    
+    def test_get_item_create_form_page_unauthorizes_user(self):
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.get(reverse('item_create_form'))
+        self.assertEqual(response.status_code, 403)
